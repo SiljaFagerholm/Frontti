43beda48caf396d05f75bd9c1bb2f23140da981f
Viljami Harjula <viljami.harjula@hotmail.com>
2018-08-02 09:27:39 +0300

    Poistettiin "vanha" sorttaus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
-import {JunkFoodComponent} from './junk-food/junk-food.component';
+import { FormsModule } from '@angular/forms';
+import { JunkFoodComponent } from './junk-food/junk-food.component';
 import { PopUpComponent } from './pop-up/pop-up.component';
-import {RouterModule} from '@angular/router';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatButtonModule} from '@angular/material/button';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatSelectModule} from '@angular/material/select';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatBottomSheetModule} from '@angular/material';
-import {MatListModule} from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatBottomSheetModule } from '@angular/material';
+import { MatListModule } from '@angular/material';
 
 import 'hammerjs';
 import '../polyfills';
@@ -26,8 +26,8 @@ import { SelectComponent } from './select/select.component';
 import { PagesComponent } from './pages/pages.component';
 import { HakuComponent } from './haku/haku.component';
 import { AlavalikkoComponent } from './alavalikko/alavalikko.component';
-import {alavalikko2 } from './alavalikko/alavalikko.component';
-import {HttpModule} from '@angular/http';
+import { alavalikko2 } from './alavalikko/alavalikko.component';
+import { HttpModule } from '@angular/http';
 import { RavintolatService } from './ravintolat.service';
 import { KayttajatService } from './kayttajat.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -47,24 +47,25 @@ import { HttpClientModule } from '@angular/common/http';
     HakuComponent,
     AlavalikkoComponent,
     alavalikko2
-    
-    
+
+
   ],
   imports: [
     BrowserModule, HttpModule, FormsModule, HttpClientModule, MatInputModule, RouterModule.forRoot([
       {
-        path:'',
-        redirectTo:'/junk-food',
-        pathMatch:'full'
+        path: '',
+        redirectTo: '/junk-food',
+        pathMatch: 'full'
       },
-      {path: 'junk-food',
-      component: JunkFoodComponent
+      {
+        path: 'junk-food',
+        component: JunkFoodComponent
       },
-      
 
-  
+
+
     ]), MatTooltipModule, MatButtonModule, BrowserAnimationsModule, MatButtonModule,
-    MatButtonToggleModule, MatGridListModule, MatSelectModule,  MatPaginatorModule, MatAutocompleteModule, 
+    MatButtonToggleModule, MatGridListModule, MatSelectModule, MatPaginatorModule, MatAutocompleteModule,
     ReactiveFormsModule, MatBottomSheetModule, MatListModule
   ],
   entryComponents: [alavalikko2],

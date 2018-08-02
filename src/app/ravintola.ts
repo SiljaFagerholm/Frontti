63513c0f249c2dac46b478ed77d4a791197c2efc
Riku Kuusinen <riku.kuusinen91@gmail.com>
2018-08-02 13:01:16 +0300

    rest toimii

---
+++ src/app/ravintola.ts
@@ -0,0 +1,14 @@
+export class Ravintola {
+    constructor(
+        public Id:number, 
+        public RavintolanNimi:string, 
+        public kayttajaId:number, 
+        public Myyjät:number, 
+        public RatingDarra:number, 
+        public RatingKanni:number, 
+        public RavintolaAvaa:string, 
+        public RavintolaSulkee:string, 
+        public Osoite:string){
+        
+    }
+}

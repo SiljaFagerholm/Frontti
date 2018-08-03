219fb8a4aa1337be33d25d1bf7db5e7dbd41d829
JukkaPeltola <jukka.peltola@mbnet.fi>
2018-08-03 09:06:16 +0300

    hökkölömmöö?

---
+++ src/app/ravintolakommentti.ts
@@ -0,0 +1,12 @@
+export class RavintolaKommentti {
+    constructor(
+        public ravKommenttiID:number, 
+        public ravintolaId:number, 
+        public kayttajaId:number, 
+        public kommentti:string, 
+        public darraRate:number,
+        public kanniRate:number, 
+        ){
+        
+    }
+}
